6e35163baea36e92c2e63f345edb47fd3b2efd09
Matt Roberts <mattrobertsky@users.noreply.github.com>
2020-06-15 14:54:33 +0100

    WIP

--- src/main/scala/code/Main.scala
+++ src/main/scala/code/Main.scala
@@ -3,18 +3,40 @@ package code
 
 case class Frame(roll1:Int, roll2:Int)
 object Frame {
-  def buildFrame(str:String):Frame = {???}
+
+  def charToDigit(ch: Char): Int =
+    ch match {
+      case '-' => 0
+      case '0' => 0
+      case '1' => 1
+      case '2' => 2
+      case '3' => 3
+      case '4' => 4
+      case '5' => 5
+      case '6' => 6
+      case '7' => 7
+      case '8' => 8
+      case '9' => 9
+    }
+
+  def build(str: String): Frame = {
+    if (str == "X")
+      Frame(10, 0)
+    else {
+      (str(0), str(1)) match {
+        case (n, '/') => Frame(charToDigit(n), 10 - charToDigit(n))
+        case (n, m) => Frame(charToDigit(n), charToDigit(m))
+      }
+    }
+  }
 }
 
 object Main extends App {
 
-
-
-
-
-
-
   def calculateScore(game:String):Int = {
+
+    game.split(" ") map Frame.build
+    
     300
   }
 
--- src/test/scala/code/MainSpec.scala
+++ src/test/scala/code/MainSpec.scala
@@ -8,4 +8,39 @@ class MainSpec extends WordSpec with Matchers {
       Main.calculateScore("X X X X X X X X X X X X") shouldBe 300
     }
   }
+
+  "A strike frame X " should {
+    "return a Frame(10,0) " in {
+      Frame.build("X") shouldBe Frame(10,0)
+    }
+  }
+
+  "A spare frame 5/ " should {
+    "return a Frame(5,5) " in {
+      Frame.build("5/") shouldBe Frame(5,5)
+    }
+  }
+
+  "A miss frame 5- " should {
+    "return a Frame(5,0) " in {
+      Frame.build("5-") shouldBe Frame(5,0)
+    }
+  }
+
+  "A miss frame -5 " should {
+    "return a Frame(0,5) " in {
+      Frame.build("-5") shouldBe Frame(0,5)
+    }
+  }
+
+  "A miss frame -- " should {
+    "return a Frame(0,0) " in {
+      Frame.build("--") shouldBe Frame(0,0)
+    }
+  }
+
+
+
+
+
 }
